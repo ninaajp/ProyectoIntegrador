8e573f2ff452005ad49136dfc7e54c3fb6ba83a6
smirandaudesa <smiranda@udesa.edu.ar>
2023-04-08 16:47:54 -0300

    product

--- parcial/routes/productos.js
+++ parcial/routes/productos.js
@@ -3,8 +3,8 @@ const router = express.Router();
 let productosController = require('../controllers/productosController');
 
 /* GET home page. */
-router.get('/product', productosController.product);
-router.get('/productAdd', productosController.productAdd);
+router.get('/detalle/:id', productosController.product); // http://localhost:3000/productos/detalle/1
+router.get('/productAdd', productosController.productAdd); // http://localhost:3000/productos/productAdd
 
 
 module.exports = router;
