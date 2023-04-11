562350909e877baf0722005dcad0e956854745a0
ninaajp <ninasoljensenpizzuti@iMac-de-Nina.fibertel.com.ar>
2023-04-11 13:15:04 -0300

    ssql

--- parcial/sql/Proyecto.sql
+++ parcial/sql/Proyecto.sql
@@ -1,22 +1,55 @@
-create database usuarios;
-use usuarios;
-create table usuarios(
-id int unsigned primary key auto_increment,
-email varchar(200) unique not null,
-usuario varchar(150) not null,
-password varchar(250) not null,
-foto varchar(200),
-fecha date,
-dni int unsigned,
-createdAt timestamp default current_timestamp,
-updatedAt timestamp default current_timestamp on update current_timestamp,
-deletedAt timestamp null
+CREATE SCHEMA aplicacion;
+
+USE aplicacion;
+
+CREATE TABLE usuarios(
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+email VARCHAR(100) NOT NULL UNIQUE,
+usuario VARCHAR(100) NOT NULL,
+password VARCHAR(250) NOT NULL,
+foto VARCHAR (100) NOT NULL,
+nacimiento DATE NOT NULL,
+DNI INT UNSIGNED,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+deletedAt TIMESTAMP NULL
 );
+
 CREATE TABLE productos(
 id int unsigned primary key auto_increment,
-foto varchar(250) unique not null,
+foto varchar(100) unique not null,
 texto varchar(250) not null,
+
 createdAt timestamp default current_timestamp,
 updatedAt timestamp default current_timestamp on update current_timestamp,
 deletedAt timestamp null
+
+FkUserId INT UNSIGNED,
+FOREIGN KEY (PkUserId) REFERENCES usuarios(id)
+
 );
+
+CREATE TABLE productos(
+id int unsigned primary key auto_increment,
+texto varchar(250) not null,
+
+createdAt timestamp default current_timestamp,
+updatedAt timestamp default current_timestamp on update current_timestamp,
+deletedAt timestamp null
+
+FkUserId INT UNSIGNED,
+FOREIGN KEY (PkUserId) REFERENCES usuarios(id)
+
+FkProductosId INT UNSIGNED,
+FOREIGN KEY (FkProductosId) REFERENCES productos(id)
+
+);
+
+INSERT INTO usuarios (email, usuario, foto, password, nacimiento, dni)
+VALUES ('Nina', 'nina@gmail.com', '/images/users/default-image.png', '1234', '2000-01-01', 43321297);
+
+INSERT INTO productos (foto, texto, FkUserId)
+VALUES ('/images/products/camisetaArg.png', 'texto descripcion', 1);
+
+INSERT INTO comentarios (texto, FkUserId)
+VALUES ('texto del comentario', 1, 1);
